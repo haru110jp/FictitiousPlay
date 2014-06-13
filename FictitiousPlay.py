9d805a13ac3d1de18eb49776a487a3585d7d559f
Atsushi Yamagishi <haru.su.jp@gmail.com>
2014-06-13 21:49:17 +0900

    Although this can generate the plausible histogram ,this code is not efficient and runs very slowly. I have to find a way to speed it up, otherwise I'll get in trouble when the game is more complex.

--- FictitiousPlay.py
+++ FictitiousPlay.py
@@ -2,54 +2,53 @@ from __future__ import division
 import matplotlib.pyplot as plt
 import random
 
-#defining variables concerning player0
-int_0 = random.uniform(0,1)
-belief00 = int_0
-trajectory0 = [int_0]
-action0 = []
+
+for k in range(1000): # you can change this number if you like
+
+	samples = []
+	
+	#defining variables concerning player0
+	int_0 = random.uniform(0,1)
+	belief00 = int_0
+	action0 = []
 
 
-int_1 = random.uniform(0,1)
-belief10 = int_1
-trajectory1 = [int_1]
-action1 = []
+	int_1 = random.uniform(0,1)
+	belief10 = int_1
+	action1 = []
 
 
-# simulating 1's actions
-for i in range(1000):
+	# simulating 1's actions
+	for i in range(1000):
 	
 	#calculating expected values
-	ev00 = belief00*1 + (1 - belief00)*(-1)
-	ev10 = belief10*(-1) + (1 - belief10)*1
+		ev00 = belief00*1 + (1 - belief00)*(-1)
+		ev10 = belief10*(-1) + (1 - belief10)*1
 	
-	if ev00 > 0:
-		action0.append(0)
-	elif ev00 < 0:
-		action0.append(1)
-	else:
-		action0.append(random.randint(0,1))
+		if ev00 > 0:
+			action0.append(0)
+		elif ev00 < 0:
+			action0.append(1)
+		else:
+			action0.append(random.randint(0,1))
 
-	if ev10 > 0:
-		action1.append(0)
-	elif ev10 < 0:
-		action1.append(1)
-	else:
-		action1.append(random.randint(0,1))
+		if ev10 > 0:
+			action1.append(0)
+		elif ev10 < 0:
+			action1.append(1)
+		else:
+			action1.append(random.randint(0,1))
 		
 	# updating beliefs
-	belief10 = belief10 + (action0[i] - belief10) / (i + 2)
-	trajectory1.append(belief10)
+		belief10 = belief10 + (action0[i] - belief10) / (i + 2)
 		
-	belief00 = belief00 + (action1[i] - belief00) / (i + 2)
-	trajectory0.append(belief00)
+		belief00 = belief00 + (action1[i] - belief00) / (i + 2)
 
+		samples.append(belief00)
 
-
-plt.plot(trajectory0, 'b-', label='player0')  
-plt.plot(trajectory1, 'r-', label='player1')  
-plt.legend()
-plt.savefig("fictitious0.png",bbox_inches="tight",pad_inches=0)
-
+fig, axes = plt.subplots(figsize=(8, 12))
+axes.hist(samples, alpha=0.6, bins=20)
+plt.savefig("fictitious_hist.png",bbox_inches="tight",pad_inches=0)
 plt.show()
 
 
