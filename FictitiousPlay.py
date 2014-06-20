5ebbbc6641b49b1db70075abfe5d8562ff34265b
Atsushi Yamagishi <haru.su.jp@gmail.com>
2014-06-20 23:21:16 +0900

    defined a lot of functions, but somehow this doesn't work as I intended.....

--- FictitiousPlay.py
+++ FictitiousPlay.py
@@ -1,54 +1,69 @@
 from __future__ import division
 import matplotlib.pyplot as plt
-import random
+import random 
+import numpy as np
 
-#defining variables concerning player0
-int_0 = random.uniform(0,1)
-belief00 = int_0
-trajectory0 = [int_0]
+
+
+#defining variables and functions that are useful
+
+payoff_0 = np.array([[1,-1],[-1,1]])
+payoff_1 = np.array([[-1,1],[1,-1]])
+
+def set_intbelief():
+	int_belief = random.uniform(0,1)
+	return  np.array([int_belief,1-int_belief])
+	 # belief about the opponent's actions
+
+belief0 = set_intbelief()
+belief1 = set_intbelief()
+
+
+def expected_value(payoff,beliefs):
+	"""
+	payoff is a matrix like np.array([[1,-1][-1,1]])
+	beliefs is a vector
+	"""
+	return np.dot(payoff,beliefs)
+	# returns expected values of each action as a vector
+
+def take_action(x,y): # this takes a vector and a list as  arguments
+	 
+	if x[0] > x[1]:
+		y.append(0)
+	elif x[0] < x[1]:
+		y.append(1)
+	else:
+		y.append(random.randint(0,1))
+
+# lists used later to draw the graph
+trajectory0 = [belief0[0]]
 action0 = []
 
-
-int_1 = random.uniform(0,1)
-belief10 = int_1
-trajectory1 = [int_1]
+trajectory1 = [belief0[0]]
 action1 = []
 
 
-# simulating 1's actions
+# simulating  actions
 for i in range(1000):
 	
-	#calculating expected values
-	ev00 = (1-belief00)*1 + belief00*(-1)
-	ev10 = (1-belief10)*(-1) + belief10*1
+	ev0 = expected_value(payoff_0,belief0)
+	ev1 = expected_value(payoff_1,belief1)
 	
-	if ev00 > 0:
-		action0.append(0)
-	elif ev00 < 0:
-		action0.append(1)
-	else:
-		action0.append(random.randint(0,1))
+	take_action(x = ev0,y = action0)
+	take_action(x = ev1,y = action1)
 
-	if ev10 > 0:
-		action1.append(0)
-	elif ev10 < 0:
-		action1.append(1)
-	else:
-		action1.append(random.randint(0,1))
-		
 	# updating beliefs
-	belief10 = belief10 + (action0[i] - belief10) / (i + 2)
-	trajectory1.append(belief10)
-		
-	belief00 = belief00 + (action1[i] - belief00) / (i + 2)
-	trajectory0.append(belief00)
+	belief0 = belief0 + (np.array([0,action1[i]]) - belief0)/(i + 2)
+	belief1 = belief1 + (np.array([0,action0[i]]) - belief1)/(i + 2)
+	trajectory0.append(belief0[0])
+	trajectory1.append(belief1[0])
 
 
 
 plt.plot(trajectory0, 'b-', label='player0')  
-plt.plot(trajectory1, 'r-', label='player1')  
+plt.plot(trajectory1, 'y-', label='player1')  
 plt.legend()
-plt.savefig("fictitious0.png",bbox_inches="tight",pad_inches=0)
 
 plt.show()
 
