1f57c1221c19551decceb49aeccae3735e20a5a8
Atsushi Yamagishi <haru.su.jp@gmail.com>
2014-06-21 10:24:40 +0900

    This can draw the histogram

--- FictitiousPlay.py
+++ FictitiousPlay.py
@@ -1,55 +1,71 @@
 from __future__ import division
 import matplotlib.pyplot as plt
-import random
+import random 
+import numpy as np
 
-samples = []
 
 
-for k in range(1000): # you can change this number if you like
+#defining variables and functions that are useful
 
+payoff_0 = np.array([[1,-1],[-1,1]])
+payoff_1 = np.array([[-1,1],[1,-1]])
+
+def set_intbelief():
+	int_belief = random.uniform(0,1)
+	return  np.array([1-int_belief,int_belief])
+	 # belief about the opponent's actions
+
+
+def expected_value(payoff,beliefs):
+	"""
+	payoff is a matrix like np.array([[1,-1][-1,1]])
+	beliefs is a vector
+	"""
+	return np.dot(payoff,beliefs)
+	# returns expected values of each action as a vector
+
+def take_action(x,y): # this takes a vector and a list as  arguments
+	 
+	if x[0] > x[1]:
+		y.append(0)
+	elif x[0] < x[1]:
+		y.append(1)
+	else:
+		y.append(random.randint(0,1))
+
+# lists used later to draw the histogram
+action0 = []
+action1 = []
+histo0 = []
+
+
+# simulating  actions
+for x in range(100):
 	
-	#defining variables concerning player0
-	int_0 = random.uniform(0,1)
-	belief00 = int_0
 	action0 = []
-
-
-	int_1 = random.uniform(0,1)
-	belief10 = int_1
 	action1 = []
-
-
-	# simulating 1's actions
+	belief0 = set_intbelief()
+	belief1 = set_intbelief()
+	
 	for i in range(1000):
-	
-	#calculating expected values
-		ev00 = (1-belief00)*1 + belief00*(-1)
-		ev10 = (1-belief10)*(-1) + belief10*1
-	
-		if ev00 > 0:
-			action0.append(0)
-		elif ev00 < 0:
-			action0.append(1)
-		else:
-			action0.append(random.randint(0,1))
-
-		if ev10 > 0:
-			action1.append(0)
-		elif ev10 < 0:
-			action1.append(1)
-		else:
-			action1.append(random.randint(0,1))
 		
-	# updating beliefs
-		belief10 = belief10 + (action0[i] - belief10) / (i + 2)
-		
-		belief00 = belief00 + (action1[i] - belief00) / (i + 2)
+		ev0 = expected_value(payoff_0,belief0)
+		ev1 = expected_value(payoff_1,belief1)
+	
+		take_action(x = ev0,y = action0)
+		take_action(x = ev1,y = action1)
 
-	samples.append(belief00)
+		# updating beliefs
+		m = belief0[1] + (action1[i] - belief0[1])/(i + 2)
+		n = belief1[1] + (action0[i] - belief1[1])/(i + 2)
+		belief0 = np.array([1-m,m])
+		belief1 = np.array([1-n,n])
+	
+	histo0.append(belief0[1])
+
 
 fig, axes = plt.subplots()
-axes.hist(samples)
-
+axes.hist(histo0)
 plt.show()
 
 
